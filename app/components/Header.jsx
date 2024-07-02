996c5f91199ce325c3dff60598f512369c2a6bec
zano <zano@live.cl>
2024-07-02 17:46:43 +0700

    update

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -4,12 +4,12 @@ import user from '../data/user.json';
 
 export const Header = () => {
 
-    const { name, username, profileImage } = user.header;
+    const { name, username } = user.header;
 
     return (
         <div className={styles.header}>
             <div>
-                <p>Nombre de usuario</p>
+                <p>Usuario</p>
                 <h2>
                     {name}
                 </h2>
